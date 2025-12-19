82de3efd5cc574031924edeba8ab39b5f866d2bc
Sergei Vakunov <bakunobu@gmail.com>
2025-12-19 16:25:07 +0300

    basic functions

--- db.py
+++ db.py
@@ -1,5 +1,61 @@
 import sqlite3
 
+class DataBaseOperator():
+    def __init__(
+            self,
+            filepath,
+            problems_name:str='problems',
+            problems_schema:str="""
+            id INTEGER PRIMARY KEY,
+            dt TEXT NOT NULL,
+            difficulty TEXT
+            """,
+            ranking_name:str='ranking',
+            ranking_schema:str="""
+            id INTEGER PRIMARY KEY,
+            dt TEXT NOT NULL,
+            user_rank INTEGER
+"""
+            ):
+        self.filepath = filepath
+        self.problems_table_name = problems_name
+        self.problems_table_schema = problems_schema
+        self.ranking_table_name = ranking_name
+        self.tanking_tble_schema = ranking_schema
+
+
+    def connect_to_db(self):
+        self.conn = sqlite3.connect(self.filepath)
+
+    def disconnect_from_db(self):
+        self.conn.close()
+
+    def write_to_db(self, tablename, tableschema, values):
+        self.connect_to_db()
+        cursor = self.conn.cursor()
+        cursor.execute(f'''INSERT INTO {tablename} ({tableschema}) VALUES ({values})''')
+        self.conn.commit()
+        self.disconnect_from_db()
+
+    def write_to_ranking(self, date, rank):
+        ...
+
+    def write_to_problems(self, date, problem_lvl):
+        ...
+
+    def generate_report(self):
+        ...
+
+    def create_empty_table(self, tablename, tableschema):
+        self.connect_to_db()
+        cursor = self.conn.cursor()
+        cursor.execute(f'''CREATE TABLE IF NOT EXISTS {tablename} ({tableschema})''')
+        self.conn.commit()
+        self.disconnect_from_db()
+
+
+import sqlite3
+
 conn = sqlite3.connect('leetcode_data.db')
 
 cursor = conn.cursor()
@@ -8,7 +64,7 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS problems (
                id INTEGER PRIMARY KEY,
                dt TEXT NOT NULL,
-               difficulty text
+               difficulty TEXT
         )
     ''')
 
@@ -23,3 +79,16 @@ INSERT INTO problems (dt, difficulty) VALUES
 )
 
 conn.commit()
+
+def connect_to_db(db_file):
+    pass
+
+
+def add_new_record(
+        task_lvl:str,
+        date:str,
+        table_name:str='problems',
+        schema:str='(dt, difficulty)',
+        connection=None
+        ):
+    cursor = conn.cursor()
